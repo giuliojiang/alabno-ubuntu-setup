d75ec8e478c2d22c40e031517ca941524edbf999
Giulio Jiang <giuliojiang@gmail.com>
2017-01-08 15:03:16 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 apt update
 apt-get -y upgrade
 apt-get -y install git make maven ghc openjdk-8-jdk python python3 python-pip python-setuptools screen cabal-install
+cabal update
 cabal install criterion hlint timeit
