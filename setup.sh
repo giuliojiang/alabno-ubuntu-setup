ffa710518fefc3de9b1828b0c8361e6d36fb957a
giuliojiang <giuliojiang@gmail.com>
2017-01-09 21:02:28 +0000

    installing criterion from the ubuntu repositories

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt-get -y upgrade
-sudo apt-get -y install git make maven ghc openjdk-8-jdk python python3 python-pip python-setuptools screen cabal-install letsencrypt
+sudo apt-get -y install git make maven ghc openjdk-8-jdk python python3 python-pip python-setuptools screen cabal-install letsencrypt libghc-criterion-dev
 cabal update
 cabal install happy
-cabal install criterion hlint timeit
+cabal install hlint timeit
