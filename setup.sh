4532026155080c2c008580f6ee9a12915863cd57
giuliojiang <giuliojiang@gmail.com>
2017-01-09 20:57:45 +0000

    adding sudo so that it doesn't have to be run as root

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-apt update
-apt-get -y upgrade
-apt-get -y install git make maven ghc openjdk-8-jdk python python3 python-pip python-setuptools screen cabal-install letsencrypt
+sudo apt update
+sudo apt-get -y upgrade
+sudo apt-get -y install git make maven ghc openjdk-8-jdk python python3 python-pip python-setuptools screen cabal-install letsencrypt
 cabal update
 cabal install happy
 cabal install criterion hlint timeit
