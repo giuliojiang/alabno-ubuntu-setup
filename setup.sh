ce5cc14bd26559eea6faa5656fc14af8a8fad335
Giulio Jiang <giuliojiang@gmail.com>
2017-01-12 10:18:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt-get -y upgrade
-sudo apt-get -y install git make maven ghc openjdk-8-jdk python python3 python-pip python-setuptools screen cabal-install letsencrypt libghc-criterion-dev
+sudo apt-get -y install git make maven ghc openjdk-8-jdk python python3 python-pip python-setuptools screen cabal-install letsencrypt libghc-criterion-dev hlint
 cabal update
 cabal install happy
-cabal install hlint timeit
+cabal install timeit
